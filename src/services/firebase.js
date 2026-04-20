304c917377042c5e1d558fb9f2dbb17d42999dc2
yeswanthkotikalapudi36 <kavyasri.marina@gmail.com>
2026-04-20 09:25:51 +0530

    fixing firebase exports

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,3 +1,22 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDrJOZ489C6jUqXy-MnAtcty-Lq0Mr96To",
+//   authDomain: "smart-travel-planner-7fc35.firebaseapp.com",
+//   projectId: "smart-travel-planner-7fc35",
+//   storageBucket: "smart-travel-planner-7fc35.firebasestorage.app",
+//   messagingSenderId: "42705037348",
+//   appId: "1:42705037348:web:1d649490848bb2ab1010b1",
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
+// export default app;
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -13,6 +32,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+// Ikkada individual ga export chestunnam
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export default app;
+
+// default export auth ni pedithe chala files ki easy ga untundi
+export default auth;
